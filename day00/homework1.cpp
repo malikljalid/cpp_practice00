f8b569b9d648a7ddce4ce0a751338013f01d5931
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-16 13:31:30 +0100

    cpp hello world practice

---
+++ day00/homework1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "***************************\n";
+    std::cout << "Name    : Zakaria Adriouche\n";
+    std::cout << "Age     : 24 Years Old\n";
+    std::cout << "City    : Casablanca\n";
+    std::cout << "Country : Morocco\n";
+    std::cout << "***************************\n";
+
+    std::cout << std::endl << std::endl;
+    std::cout << "*******\n";
+    std::cout << "*******\n";
+    std::cout << "*******\n";
+    std::cout << "*******\n";
+
+
+    std::cout << std::endl << std::endl;
+    std::cout << "**********************************\n";
+    std::cout << "I Love Programming !" << std::endl;
+    std::cout << "I promise to be the best developer EVER!!" << std::endl;
+    std::cout << "I know it will take sometime to practice, but i will achieve my GOAL !!" << std::endl;
+    std::cout << "Best regards,\nZakaria Adriouche." << std::endl;
+    std::cout << "***********************************\n";
+
+    std::cout << std::endl << std::endl;
+    std::cout << "*    *\n";
+    std::cout << "*    *\n";
+    std::cout << "******\n";
+    std::cout << "*    *\n";
+    std::cout << "*    *\n";
+    
+    std::cout << "\nZakaria is LEARNING QUICKLY Slaay !!!!\n";
+
+    return (0);
+}
