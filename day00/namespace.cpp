fbb026dde5126a78810f855ad342b0c5fdc79570
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-16 13:32:01 +0100

    what the hell is a namespace

---
+++ day00/namespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "ZAKARIA ADRIOUCHE is learning very quicklyyy !! " << endl;
+    cout << "ZAKARIA ADRIOUCHE is learning very quicklyyy !! " << endl;
+    return (0);
+}
