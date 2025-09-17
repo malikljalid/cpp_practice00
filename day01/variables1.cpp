8f80928d5310b8d27ba0c2e9aefc0bd8e29354c7
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-17 12:39:38 +0100

    intro into variables

---
+++ day01/variables1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void)
+{
+    int Age = 42;
+    char alpha = 'z';
+
+    float Grade = 13.5;
+    double H = 10555.5555;
+
+    bool Valid = true;
+    std::string Name = "Nano";
+
+    std::cout << "let's show them all : " << Age << '|' << alpha << '|' << Grade << '|' << H << '|' << Valid << '|' << Name << '\n';
+
+
+    return (0);
+}
