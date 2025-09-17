3db01c5e3cb2221b3b1c8e6d09d892b7053e522e
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-17 09:14:11 +0100

    so the consntants we use in consol output called literals

---
+++ day01/literals.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello " << 42 << " !\n";
+    std::cout << "Messi is better than Ronaldo " << 10.5 << " Times.\n";
+    std::cout << "this is : " << 'c' << ", is it?\n";
+    std::cout << "i wish i was in this era " << 0x0f << " in the memory!\n";
+    std::cout << "CAN I SEE THIS ? --> " << 0777 << "? \n";
+
+    return (0);
+}
