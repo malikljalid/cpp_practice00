a220cf46b5160e3a64ceaab4822acb3b3626d252
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-17 12:41:05 +0100

    even if you tried reverse them in the console lol

---
+++ day01/variables3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+    char A, B, C;
+
+    A = 'y';
+    B = 'b';
+    C = 'w';
+
+    std::cout << A << B << C << " | " << C << B << A;
+    
+    return (0);
+}
