e313fbd6bf4cd79c33270a686e5b5c59ff859111
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-17 15:48:27 +0100

    math op using variables

---
+++ day01/ex04.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+    int A;
+    int B;
+    int C;
+
+    A = 20;
+    B = 30;
+    C = 10;
+
+    std::cout << A << '+' << '\n';
+    std::cout << B << '+' << '\n';
+    std::cout << C << '\n';
+    std::cout << "___\n";
+    std::cout << A + B + C << '\n';
+    return (0);
+}
