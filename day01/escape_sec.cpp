27f2819f3e14e62ff9d4cacd90fbd70f20e2ac1f
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-17 09:23:04 +0100

    escape sequences are cool

---
+++ day01/escape_sec.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello World\n";
+    std::cout << "Hello \tZakaria\n";
+    std::cout << "his name is \"Zachariah\" \n";
+    std::cout << "he is born in 2025\\09\\01 \n";
+    std::cout << "his favorite tool is c. \n";
+    std::cout << "maybe he is a magic user! \a  \n";
+
+    return (0);
+}
