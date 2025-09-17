77f226cd8bd7ca1958b11b1da3173a0d4b457fc5
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-17 09:33:26 +0100

    Little escape practice

---
+++ day01/ex01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello Sir/Madame.\n";
+    std::cout << "How are you?\n";
+    std::cout << "My name is \"Zacharia\", i am fine thank you!\n"; 
+
+    return (0);
+}
