8b5754ee7d0819c402ed4b7a97ee59f2d926a157
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-22 18:12:48 +0100

    half of a number ex

---
+++ day02/ex07.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+    int A;
+
+    std::cout << "Please insert a number : ";
+    std::cin >> A;
+
+    std::cout << "Half of " << A <<" is : " << A/2 << '\n'; 
+    return (0);
+}
