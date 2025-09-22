5aab297e0aaf9fcdff426d13d1c12624b45359a7
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-22 09:58:19 +0100

    arithemtic operations in cpp

---
+++ day02/arithmetic_operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void)
+{
+    int A, B;
+
+    A = 20;
+    B = 10;
+    
+    std::cout << "A + B = " << A + B << '\n';
+    std::cout << "A - B = " << A - B << '\n';
+    std::cout << "A * B = " << A * B << '\n';
+    std::cout << "A / B = " << A / B << '\n';
+    std::cout << "A % B = " << A % B << '\n';
+    
+    return (0);
+}
