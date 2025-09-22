5e4eeea5695e308095f959c63a58f07321600170
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-22 09:58:49 +0100

    practicing arithmetic operations in cpp

---
+++ day02/ex00.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void)
+{
+    int A, B;
+
+    std::cout << "Please give a number A : ";
+    std::cin >> A;
+    std::cout << "Please give a number B : ";
+    std::cin >> B;
+    std::cout << "A + B = " << A + B << '\n';
+    std::cout << "A - B = " << A - B << '\n';
+    std::cout << "A * B = " << A * B << '\n';
+    std::cout << "A / B = " << A / B << '\n';
+    std::cout << "A % B = " << A % B << '\n';
+    
+    return (0);
+}
