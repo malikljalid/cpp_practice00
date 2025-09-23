f7052196cd295ffff8d8bdf01c1cf8a676b097b7
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-23 09:48:49 +0100

    intro to increment operations

---
+++ day03/increment_operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+    int A = 10;
+    int B = 20;
+
+    A++;  //equal to A = A + 1
+    B--;  //equal to B = B + 1
+
+    std::cout << "A = " << A << std::endl;
+    std::cout << "B = " << B << std::endl;
+
+    return (0);
+}
