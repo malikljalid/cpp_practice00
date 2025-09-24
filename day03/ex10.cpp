041d25420e449fd3c200c986bcf61f4096523f34
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-24 10:18:11 +0100

    power of 2, 3 and 4 for a number using math library

--- day03/ex10.cpp
+++ day03/ex10.cpp
@@ -8,9 +8,9 @@ int main(void)
     std::cout << "Please enter a number : ";
     std::cin >> Number;
 
-    std::cout << Number << "^ 2 = " << pow(Number, 2) << '\n';
-    std::cout << Number << "^ 3 = " << pow(Number, 3) << '\n';
-    std::cout << Number << "^ 4 = " << pow(Number, 4) << '\n';
+    std::cout << Number << "^2 = " << pow(Number, 2) << '\n';
+    std::cout << Number << "^3 = " << pow(Number, 3) << '\n';
+    std::cout << Number << "^4 = " << pow(Number, 4) << '\n';
 
     return (0);
 }
