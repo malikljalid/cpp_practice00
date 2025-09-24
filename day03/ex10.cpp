c47651a6c2a7f4ec2aaa11129a2e5a0dc504687a
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-24 10:12:27 +0100

    power of 2, 3 and 4 for a numbe using math library

---
+++ day03/ex10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+    int Number;
+
+    std::cout << "Please enter a number : ";
+    std::cin >> Number;
+
+    std::cout << Number << "^ 2 = " << pow(Number, 2) << '\n';
+    std::cout << Number << "^ 3 = " << pow(Number, 3) << '\n';
+    std::cout << Number << "^ 4 = " << pow(Number, 4) << '\n';
+
+    return (0);
+}
