d7c89a2cb6b532e9f2d4f08b73d0cfb592cd0f96
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-26 10:26:15 +0100

    quick fix on printing the swaped numbers using a procedure

--- day05/ex14.cpp
+++ day05/ex14.cpp
@@ -4,6 +4,9 @@ void swapTwoNumbers(int N1, int N2)
 {
     int tmp;
 
+    std::cout << "Number 1 = " << N1 << std::endl;
+    std::cout << "NUmber 2 = " << N2 << std::endl;
+
     tmp = N1;
     N1  = N2;
     N2  = tmp;
@@ -16,10 +19,10 @@ int main(void)
 {
     int N1, N2;
 
-    std::cout << "Please enter Number 1 :";
+    std::cout << "Please enter Number 1 : ";
     std::cin >> N1;
 
-    std::cout << "Please enter Number 2 :";
+    std::cout << "Please enter Number 2 : ";
     std::cin >> N2;
 
     swapTwoNumbers(N1, N2);
