25e36360583a79d20639afe925f20ca7a25b786a
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-26 10:24:18 +0100

    swap two numbers using a procedure

---
+++ day05/ex14.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void swapTwoNumbers(int N1, int N2)
+{
+    int tmp;
+
+    tmp = N1;
+    N1  = N2;
+    N2  = tmp;
+
+    std::cout << "Number 1 = " << N1 << std::endl;
+    std::cout << "NUmber 2 = " << N2 << std::endl;
+}
+
+int main(void)
+{
+    int N1, N2;
+
+    std::cout << "Please enter Number 1 :";
+    std::cin >> N1;
+
+    std::cout << "Please enter Number 2 :";
+    std::cin >> N2;
+
+    swapTwoNumbers(N1, N2);
+    return (0);
+}
