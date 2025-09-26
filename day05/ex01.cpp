4837ad8ccfb8f8870cd75d9b34a114b6e5959877
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-26 10:12:12 +0100

    print my name on console

---
+++ day05/ex01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void sayMyName(void)
+{
+    std::cout << "Zakaria Adriouche \n";
+}
+
+int main(void)
+{
+    sayMyName();
+    sayMyName();
+    sayMyName();
+    sayMyName();
+    sayMyName();
+    sayMyName();
+    
+    return (0);
+}
