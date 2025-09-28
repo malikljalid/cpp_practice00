e2566ec3cd5ea8741557839e4b31d21d23ac4cf7
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-28 23:56:57 +0100

    the final form of if statement if elseif else discovered

--- day06/if_elseif_else.cpp
+++ day06/if_elseif_else.cpp
@@ -6,14 +6,17 @@ int main(void)
 
     if (time < 10)
     {
+        // it will enter here if condition1 is true
         std::cout << "Good Morning!\n";
     }
     else if (time < 14)
     {
+        // it will enter here if condition1 is false and cond2 is true
         std::cout << "Good afternon!\n";
     }
     else
     {
+        // it will enter here if nothing above was true
         std::cout << "Good Night!\n";
     }
 
