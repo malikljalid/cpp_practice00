ea228895a5e96e4a5449a562e994758b0790075e
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-28 21:03:57 +0100

    if statement discovered, its cool btw

---
+++ day06/if.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+    int A;
+
+    A = 6;
+
+    if (A < 13)
+    {
+        std::cout << "We are inside the if statement, Wow!\n";
+    }
+    std::cout << "We just skipped the if statement\n";
+    return (0);
+}
