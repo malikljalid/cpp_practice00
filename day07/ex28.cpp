b5d54201a2757edef282e7fc81733f3e37671f84
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-30 09:34:48 +0100

    getting sum of odd numbers from 1 to N using for loop

---
+++ day07/ex28.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(void)
+{
+    int N;
+    int Sum = 0;
+
+    std::cout << "Please enter N : ";
+    std::cin >> N;
+    
+    for (int i = 1; i <= N ; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Sum += i;
+        }
+    }
+    std::cout << "Sum of ODD Number from 1 to " << N << " is = " << Sum << std::endl;
+    return (0);
+}
