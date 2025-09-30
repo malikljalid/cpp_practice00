a1b81772bc6d889f0ac60e1385f653f9c202fdea
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-30 09:35:05 +0100

    getting sum of odd numbers from 1 to N using for loop + fcts

---
+++ day07/ex28_smart.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct stInput
+{
+    int N;
+    int Sum;
+};
+
+void readInput(stInput &Input)
+{
+    std::cout << "Please enter N : ";
+    std::cin >> Input.N;
+    Input.Sum = 0;
+}
+
+void  printSumOfNumberFrom1toN(stInput &Input)
+{
+    for (int i = 1; i <= Input.N ; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Input.Sum += i;
+        }
+    }
+    std::cout << "Sum of ODD Number from 1 to " << Input.N << " is = " << Input.Sum << std::endl;
+}
+
+int main(void)
+{
+    stInput Input;
+
+    readInput(Input);
+    printSumOfNumberFrom1toN(Input);
+
+    return (0);
+}
