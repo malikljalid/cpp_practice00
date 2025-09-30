4b436df14eaf62f582ed77369fbe62e7d6cc79f3
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-09-30 12:32:58 +0100

    printing all alphabete characters from A to Z using for loop

---
+++ day07/ex46.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+    int ASCII = 65;
+
+    for (int ASCII = 65; ASCII <= 90; ASCII++)
+    {
+        std::cout << char(ASCII) << std::endl;
+    }
+    return (0);
+}
