b6928667f038ce9ad7858aac90b3247247c31417
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-01 13:24:28 +0100

    listing numbers from N to 1 using while loop

---
+++ day07/ex27_while.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void readN(int &N)
+{
+    std::cout << "Please enter N : ";
+    std::cin >> N;
+}
+
+void printFromNTo1(int N)
+{
+    int i = N;
+
+    while (i >= 1)
+    {
+        std::cout << i << '\n';
+        i++;
+    }
+}
+
+int main(void)
+{
+    int N;
+
+    readN(N);
+    printFromNTo1(N);
+
+    return (0);
+}
