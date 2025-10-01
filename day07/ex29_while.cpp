d89366a772b5a933a6d48281ab7d691c53418644
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-01 13:28:42 +0100

    sum of odd numbers from 1 to N using while loop

---
+++ day07/ex29_while.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+struct stInput
+{
+    int N;
+    int Sum;
+};
+
+void readInput(stInput &Input)
+{
+    std::cout << "Please enter N : ";
+    std::cin >> Input.N;
+    Input.Sum = 0;
+}
+
+void  printSumOfOddNumberFrom1toN(stInput &Input)
+{
+    int i = 1;
+
+    while (i <= Input.N)
+    {
+        if (i % 2 != 0)
+        {
+            Input.Sum += i;
+        }
+        i++;
+    }
+    std::cout << "Sum of ODD Number from 1 to " << Input.N << " is = " << Input.Sum << std::endl;
+}
+
+int main(void)
+{
+    stInput Input;
+
+    readInput(Input);
+    printSumOfOddNumberFrom1toN(Input);
+
+    return (0);
+}
