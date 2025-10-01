9c1642b6b3cb8f59b2a3272ae829755910ee87c2
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-01 11:25:15 +0100

    traingle down to top using 2 nested for loops

--- day07/for_nested5.cpp
+++ day07/for_nested5.cpp
@@ -4,7 +4,7 @@ int main(void)
 {
     for (int i = 10; i >= 1; i--)
     {
-        for (int j = 10; j >= i; j--)
+        for (int j = 1; j <= i; j++)
         {
             std::cout << "*";
         }
